c907b3b4cb54ecc409ddbfa6e2542c90ae04c17f
Robert Müller 🚀 <code@brauser.io>
2019-10-24 20:20:24 +0200

    fix double goals; go rewrite
    Signed-off-by: Robert Müller 🚀 <code@brauser.io>

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM arm32v7/ruby:alpine
-ADD . /
-RUN apk add --update \
-    build-base \
-  && gem install epoll \
-  && rm -rf /var/cache/apk/*
-CMD ruby /gpio.rb
---
+++ goal.go
@@ -0,0 +1,92 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"github.com/warthog618/gpio"
+)
+
+var (
+	mutex *sync.Mutex
+        redGoalLastState gpio.Level
+        whiteGoalLastState gpio.Level
+)
+
+
+func watchRedGoal(pin *gpio.Pin) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	currentState := pin.Read()
+	if redGoalLastState == true && currentState == false {
+		fmt.Println("White scored")
+
+		_, err := http.PostForm("http://localhost:3000/goals", url.Values{"team": {"white"}})
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(2 * time.Second)
+	}
+	redGoalLastState = currentState
+}
+
+func watchWhiteGoal(pin *gpio.Pin) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	currentState := pin.Read()
+	if whiteGoalLastState == true && currentState == false {
+		fmt.Println("Red scored")
+
+		_, err := http.PostForm("http://localhost:3000/goals", url.Values{"team": {"red"}})
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(2 * time.Second)
+	}
+	whiteGoalLastState = currentState
+}
+
+func main() {
+	mutex = &sync.Mutex{}
+
+	err := gpio.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer gpio.Close()
+	redGoal := gpio.NewPin(17)
+	redGoal.Input()
+	redGoal.PullUp()
+	whiteGoal := gpio.NewPin(27)
+	whiteGoal.Input()
+	whiteGoal.PullUp()
+
+	// capture exit signals to ensure resources are released on exit.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, os.Kill)
+	defer signal.Stop(quit)
+
+	err = redGoal.Watch(gpio.EdgeRising, watchRedGoal)
+	if err != nil {
+		panic(err)
+	}
+	defer redGoal.Unwatch()
+
+	err = whiteGoal.Watch(gpio.EdgeRising, watchWhiteGoal)
+	if err != nil {
+		panic(err)
+	}
+	defer whiteGoal.Unwatch()
+
+	fmt.Println("Watching Pin goals...")
+	select {
+	case <-quit:
+	}
+
+}
