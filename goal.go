9ed6802c0838b7239650c51f7a0af87b7240dfca
Robert Müller 🚀 <code@brauser.io>
2019-11-06 20:00:58 +0100

    clean up
    Signed-off-by: Robert Müller 🚀 <code@brauser.io>

--- goal.go
+++ goal.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
@@ -25,13 +24,10 @@ func watchRedGoal(pin *gpio.Pin) {
 
 	currentState := pin.Read()
 	if redGoalLastState == true && currentState == false {
+		// if red goal callback is triggered team white scored
 		fmt.Println("White scored")
 		client.Publish(goalTopic, 0, false, "white")
 
-		_, err := http.PostForm("http://localhost:3000/goals", url.Values{"team": {"white"}})
-		if err != nil {
-			fmt.Println(err)
-		}
 		time.Sleep(2 * time.Second)
 	}
 	redGoalLastState = currentState
@@ -43,34 +39,37 @@ func watchWhiteGoal(pin *gpio.Pin) {
 
 	currentState := pin.Read()
 	if whiteGoalLastState == true && currentState == false {
+		// if  white goal callback is triggered team red scored
 		fmt.Println("Red scored")
 		client.Publish(goalTopic, 0, false, "red")
-
-		_, err := http.PostForm("http://localhost:3000/goals", url.Values{"team": {"red"}})
-		if err != nil {
-			fmt.Println(err)
-		}
 		time.Sleep(2 * time.Second)
 	}
 	whiteGoalLastState = currentState
 }
 
 func main() {
+	// connect to mqtt broker as publisher
 	uri, err := url.Parse("mqtt://172.30.1.32:1883")
 	if err != nil {
 		log.Fatal(err)
 	}
 	client = connect("pub", uri)
+
 	mutex = &sync.Mutex{}
 
+	// try to open gpio pins
 	err = gpio.Open()
 	if err != nil {
 		panic(err)
 	}
 	defer gpio.Close()
+
+	// define pin for team red and configure it
 	redGoal := gpio.NewPin(17)
 	redGoal.Input()
 	redGoal.PullUp()
+
+	// define pin for team white and configure it
 	whiteGoal := gpio.NewPin(27)
 	whiteGoal.Input()
 	whiteGoal.PullUp()
@@ -80,12 +79,14 @@ func main() {
 	signal.Notify(quit, os.Interrupt, os.Kill)
 	defer signal.Stop(quit)
 
+	// set callback for red goal
 	err = redGoal.Watch(gpio.EdgeRising, watchRedGoal)
 	if err != nil {
 		panic(err)
 	}
 	defer redGoal.Unwatch()
 
+	// set callback for white goal
 	err = whiteGoal.Watch(gpio.EdgeRising, watchWhiteGoal)
 	if err != nil {
 		panic(err)
